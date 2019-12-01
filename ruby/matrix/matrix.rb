34852c29d85fe35977a8dd661735d99ac4238105
Kurt Apple <fake@fake.fake>
2019-11-30 17:41:51 -0800

    matrix exercise

--- ruby/matrix/matrix.rb
+++ ruby/matrix/matrix.rb
@@ -6,9 +6,17 @@ To get started with TDD, see the `README.md` file in your
 `ruby/matrix` directory.
 =end
 
-module Matrix
-	def self.new(matrix_input)
+class Matrix
+	attr_accessor :the_matrix
+	def initialize(matrix_input)
 		if matrix_input.is_a?(String)
-			matrix_input.
+			self.the_matrix = matrix_input.split("\n").map!{|row| row.split(" ")}.map!{|row| row.map!{|cell| cell.to_i}}
+		end
+	end
+	def columns
+		the_matrix.transpose
+	end
+	def rows
+		the_matrix
 	end
 end
